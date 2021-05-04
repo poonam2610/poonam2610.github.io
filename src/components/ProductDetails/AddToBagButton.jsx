cb0036917f03e428e87b60999b6c50434b86a66c
Anusha <anusha19@navgurukul.org>
2021-05-04 13:02:52 +0530

    Fixing add to bag button animation

--- src/components/ProductDetails/AddToBagButton.jsx
+++ src/components/ProductDetails/AddToBagButton.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./AddToBagButton.scss";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart} from "react-icons/fa";
+// import { FcRight } from "react-icons/fc";
 
 
 export default function AddToBagButton({content , isProductAdded, handleClick}) {
-
+ 
   return (
+    
     <div >
       <button className="outer__button" onClick={()=>{handleClick()}} >
         <div className={`icon__container ${isProductAdded? "animate" : ""}`}>
-          <FaShoppingCart />
+        {/* <div className = "icon__container" style= { isProductAdded
+            ? { transform: "translateX(0)", transition: "all 0.7s ease" }
+            : { transform: `translateX(${w}%)`}}> */}
+          <FaShoppingCart/>
         </div>
         <div className = "button__content">
         {`${isProductAdded ?"ADDED" :content}`}
